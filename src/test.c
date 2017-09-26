c3675af601364f11ce0949fedc90407f5734e00f
lapin <serprev.z@gmail.com>
2017-09-26 20:56:35 +0200

    nothing chage juste the date

--- src/test.c
+++ src/test.c
@@ -6,24 +6,64 @@
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 16:21:49 by fjanoty           #+#    #+#             */
-/*   Updated: 2017/09/21 18:15:05 by fjanoty          ###   ########.fr       */
+/*   Updated: 2017/09/26 20:47:20 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "rtv1.h"
 
-void	test_init_obj(t_obj *obj)
+
+void	set_plan_test(t_obj *obj)
 {
-	ft_bzero(obj, sizeof(obj) * 5);
-	obj[0].type = 3;
-	obj[0].radius = 4;
+	obj->type = 0;
+	vec_set(obj->ang, 0, 0, 0);		
+	vec_set(obj->pos, 0, -2, 0);		
+	vec_set(obj->dir, 0, -1, 0.1);
+	vec_normalise(obj->dir, obj->dir);
+	plan_init(obj); 					// dans radius on met le coef 'd' de l'eq du plan
+}
+
+void	set_sphere_test(t_obj *obj)
+{
+	obj->type = 1;
+	obj->radius = 1;
+	vec_set(obj->ang, 0, 0, 0);
+	vec_set(obj->pos, -2, 3, -5);
+	vec_set(obj->dir, 0, 0, 0);
+}
+
+void	set_cylindre_test(t_obj *obj)
+{
+	obj->type = 2;
+	obj->radius = 4;
 	vec_set(obj->ang, 0, 0, 0);
 	vec_set(obj->pos, 0.1, 0.1, 4);
 	vec_set(obj->dir, 1, 1, 1);
+}
 
-//	plan_init(obj);
-	obj[1].type = -1;
-//	printf("plan d:%f\n", obj[0].radius);
+void	set_cone_test(t_obj *obj)
+{
+	obj->type = 3;
+	obj->radius = 4;
+	vec_set(obj->ang, 0, 0, 0);
+	vec_set(obj->pos, 0, 0, -4);
+	vec_set(obj->dir, 1, 1, 1);
+}
+
+void	test_init_obj(t_obj *obj)
+{
+	int	i = 0;
+	
+	memset(obj, 0, sizeof(obj) * 5);
+	for (i = 0; i < 5; i++)
+		obj[i].type = -1;
+	i = 0;
+	set_plan_test(obj + i++);
+	set_sphere_test(obj + i++);
+	set_cylindre_test(obj + i++);
+	set_cone_test(obj + i++);
+
+//	obj[4].type = -1;		// la fin de la serie d'objet
 }
 
 
@@ -35,7 +75,7 @@ void	test_basique(t_env *e)
 
 	test_init_obj(obj);
 
-	fill_zbuff(e, &e->scene, &e->cam, obj);
+	fill_zbuff(&e->scene, &e->scene.cam, obj);
 	color_scene(&e->scene, obj); 
 	mlx_put_image_to_window(e->mlx, e->scene.win, e->scene.img, 0, 0);
 //	basis_describe(&e->cam);
