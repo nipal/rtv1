64ea3861e7428b3f025eab1abf1612ed1b7ff883
lapin <serprev.z@gmail.com>
2017-10-07 20:44:00 +0200

    ajout des touche appel dans les define

--- inc/key_linux.h
+++ inc/key_linux.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_linux.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/07 14:43:46 by fjanoty           #+#    #+#             */
+/*   Updated: 2017/10/07 14:58:33 by fjanoty          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef KEY_LINUX_H
 # define KEY_LINUX_H
 
@@ -8,5 +20,31 @@
 # define KEY_DOWN	65364
 # define KEY_UP		65362
 
+# define KEY_A		'a'
+# define KEY_B		'b'
+# define KEY_C		'c'
+# define KEY_D		'd'
+# define KEY_E		'e'
+# define KEY_F		'f'
+# define KEY_G		'g'
+# define KEY_H		'h'
+# define KEY_I		'i'
+# define KEY_J		'j'
+# define KEY_K		'k'
+# define KEY_L		'l'
+# define KEY_M		'm'
+# define KEY_N		'n'
+# define KEY_O		'o'
+# define KEY_P		'p'
+# define KEY_Q		'q'
+# define KEY_R		'r'
+# define KEY_S		's'
+# define KEY_T		't'
+# define KEY_U		'u'
+# define KEY_V		'v'
+# define KEY_W		'w'
+# define KEY_X		'x'
+# define KEY_Y		'y'
+# define KEY_Z		'z'
 
 #endif
--- inc/key_mac.h
+++ inc/key_mac.h
@@ -1,12 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   key_mac.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/07 14:43:56 by fjanoty           #+#    #+#             */
+/*   Updated: 2017/10/07 14:56:57 by fjanoty          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef KEY_MAC
 # define KEY_MAC
 
+# define SHIFT_L	257
+# define CTRL_L		256
+# define ALT_L		261
+# define CMD_L		259
+
+# define SHIFT_R	258
+# define CTRL_R		269
+# define ALT_R		262
+# define CMD_R		260
 
 # define KEY_ESC	53
+# define ENTER		36
+
 # define KEY_LEFT	123
 # define KEY_RIGHT	124
 # define KEY_DOWN	125
 # define KEY_UP		126
+
+
+# define KEY_A		0
+# define KEY_B		11
+# define KEY_C		8
+# define KEY_D		2
+# define KEY_E		14
+# define KEY_F		3
+# define KEY_G		5
+# define KEY_H		4
+# define KEY_I		34
+# define KEY_J		38
+# define KEY_K		40
+# define KEY_L		37
+# define KEY_M		46
+# define KEY_N		45
+# define KEY_O		31
+# define KEY_P		35
+# define KEY_Q		12
+# define KEY_R		15
+# define KEY_S		1
+# define KEY_T		17
+# define KEY_U		32
+# define KEY_V		9
+# define KEY_W		13
+# define KEY_X		7
+# define KEY_Y		16
+# define KEY_Z		6
 
 
 #endif
