51cfd3f0fd95d970ae8105bd1f6f5ff12b7efd8c
lapin <serprev.z@gmail.com>
2017-10-07 20:43:07 +0200

    archi de parsing est en place

---
+++ test/parsing/file_str.c
@@ -0,0 +1,78 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   file_str.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/07 17:32:30 by fjanoty           #+#    #+#             */
+/*   Updated: 2017/10/07 20:02:20 by fjanoty          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+
+char	*get_file_str()
+{
+	static	char	*str = NULL;
+
+	if (!str)
+	{
+		str = strdup(
+"\
+\n\
+\n\
+\n\
+light\n\
+{\n\
+	pos:	0, 0, 0\n\
+	col:	255, 255, 255, 1; # couleur + puissance...\n\
+}\n\
+\n\
+camera\n\
+{\n\
+	pos:	0, 0, 0\n\
+	up:		0, 1, 0\n\
+	dir:	0, 0, 1\n\
+}\n\
+\n\
+plan\n\
+{\n\
+	pos:	0, -2, 0\n\
+	col:	0,    206, 209\n\
+	dir:	0, -1, 0.1\n\
+}\n\
+\n\
+sphere\n\
+{\n\
+	pos:	2, 3, -3\n\
+	col:	255, 255, 255\n\
+	coef:	3\n\
+}\n\
+\n\
+cylinder\n\
+{\n\
+	pos:	2, 3, -3\n\
+	col:	239, 28, 98\n\
+	dir:	.2, 0.1, 1.0\n\
+	coef:	1\n\
+}\n\
+\n\
+cone\n\
+{\n\
+	pos:	0, -3, 5\n\
+	col:	108, 139, 61\n\
+	dir:	0, 1, 0\n\
+	coef:	1\n\
+}\n\
+\n\
+\n\
+# On eleve tout les comentaire\n\
+# On doit trouver des objet avec des parametre (float)\n\
+# Il ne sont pas necessairement tous definie\n\
+# Il peu y avoir des erreur: truc en trop, mal parametrer\n\
+# 	- on met des valeur par default, ou autre chose\n\
+#	- il peu y a voir une double definition ... on prend la dernier\n");
+	}
+	return (str);
+}
--- test/parsing/parse_rtv1.c
+++ test/parsing/parse_rtv1.c
@@ -5,54 +5,121 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/10/06 11:10:49 by fjanoty           #+#    #+#             */
-/*   Updated: 2017/10/06 15:31:02 by fjanoty          ###   ########.fr       */
+/*   Created: 2017/10/07 18:28:09 by fjanoty           #+#    #+#             */
+/*   Updated: 2017/10/07 20:42:27 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
-# define VDIM 3
+/////////////////////////////
+#include "file_str.c"
+/////////////////////////////
 
+# define VDIM 3
 
 typedef	struct	s_vec3
 {
-	float	x;
-	float	y;
-	float	z;
+	double	x;
+	double	y;
+	double	z;
 }				t_vec3;
 
-typedef	union	u_axes
+typedef	struct	s_cam
 {
-	float		comp[VDIM][VDIM];
-	float		all[VDIM * VDIM];
-}				t_axes;
+	t_vec3		pos;
+	t_vec3		ux;
+	t_vec3		uy;
+	t_vec3		uz;
+}				t_cam;
 
-typedef	struct	s_basis
+typedef	struct	s_light
 {
-	float		pos[3];
-	float		*axes[VDIM];
-	float		ux[VDIM];
-	float		uy[VDIM];
-	float		uz[VDIM];
-}				t_basis;
+	t_vec3		pos;
+	t_vec3		col;					// pour l'instant osef
+	double		power;					// poura etre une condition d'arret si negatif
+}				t_light;
+
+typedef	struct	s_mat3
+{
+	t_vec3		ux;
+	t_vec3		uy;
+	t_vec3		uz;
+}				t_mat3;
+
+typedef	struct	s_obj
+{
+	int			type;
+	t_vec3		col;					// pour l'instant comme on connais [0, 255] juste ca changera plus tard
+	t_mat3		rot_inv;
+	t_vec3		ang;	// on peut reconstruire ses transformation
+	t_vec3		pos;
+	t_vec3		dir;
+	double		value;
+}				t_obj;
+
+
+typedef	struct	s_item
+{
+	double		size_x;
+	double		size_y;
+	t_cam		*cam;					//		#CAM# on a une copie du pointeur ici qui est declarer dans t_mlx_win
+	t_light		*light;
+	t_obj		*obj;
+	int			nb_light;
+	int			nb_obj;
+	int			nb_cam;
+	double		(*obj_dist[4])(t_obj *obj, t_vec3 ray_pos, t_vec3 ray_dir);
+	t_vec3		(*obj_nrm[4])(t_obj *obj, t_vec3 pos_impact);
+}				t_item;
+
 
 int		is_space(char c)
 {
-	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r')
+	if (c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ')
 		return (1);
-	return (1);
+	return (0);
 }
 
-float	atol_size(const char *str, int *id)
+/*
+char	*skip_withe_space(char *str)
+{
+	while (is_space(*str++))
+		;
+	return (str);
+}
+*/
+
+int		skip_withe_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && is_space(str[i]))
+		i++;
+	return (i);
+}
+
+int		skip_non_space(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && !is_space(str[i]))
+		i++;
+	return (i);
+}
+
+double	atol_size(const char *str, int *id)
 {
 	int		i;
 	int		j;
-	float		nb;
-	float		factor;
+	double		nb;
+	double		factor;
 
 	if (id)
 		*id = 0;
@@ -75,12 +142,12 @@ float	atol_size(const char *str, int *id)
 	return (0);
 }
 
-int	float_is_neg(float nb)
-{
-	if (*(unsigned int*)(&nb) & 1 << 31)
-		return (1);
-	return (0);
-}
+//	int	double_is_neg(double nb)
+//	{
+//		if (*(unsigned int*)(&nb) & 1 << 31)
+//			return (1);
+//		return (0);
+//	}
 
 int	double_is_neg(double nb)
 {
@@ -89,17 +156,17 @@ int	double_is_neg(double nb)
 	return (0);
 }
 
-float	str_get_float(const char *str, int *curs)
+double	str_get_double(const char *str, int *curs)
 {
-	float 	nb;
-	float	coef;
+	double 	nb;
+	double	coef;
 	long	tmp;
 	int		i;
-	float	sign;
+	double	sign;
 
-	nb = (float)atol_size(str, &i);
-	sign = (float_is_neg(nb)) ? -1.0 : 1.0;
-	nb = (float_is_neg(nb)) ? nb * -1 : nb;	
+	nb = (double)atol_size(str, &i);
+	sign = (double_is_neg(nb)) ? -1.0 : 1.0;
+	nb = (double_is_neg(nb)) ? nb * -1 : nb;
 	*curs = i;
 	if (str[i] && str[i] == '.'  && str[i + 1] >= '0' && str[i + 1] <= '9')
 	{
@@ -121,11 +188,11 @@ t_vec3	str_get_vec(char *str)
 	return (v);
 }
 
-void	test_get_float(const char *str)
+void	test_get_double(const char *str)
 {
 	int	i;
 
-	printf("%s -> %.20f\n", str, str_get_float(str, &i));
+	printf("%s -> %.20f\n", str, str_get_double(str, &i));
 	printf("%s -> %.20f\n", str, atof(str));
 }
 
@@ -145,11 +212,11 @@ void	print_bit(ul toto, int val)
 }
 
 
-void	test_float()
+void	test_double()
 {
 //	double	toto = -0.0;
-//	
-//	if (float_is_neg(toto))
+//
+//	if (double_is_neg(toto))
 //		printf("Negative\n");
 //	else
 //		printf("positive\n");
@@ -164,15 +231,15 @@ void	test_float()
 //	printf("-0.0 => %ld\n", *((long*)(&toto)));
 //	printf("0.0  => %ld\n", *((long*)(&toto)));
 
-	test_get_float("-0.01234567890123456789 ");
-//	test_get_float("-0.09868798798798 ");
-//	test_get_float(" -0978  ");
-//	test_get_float("0.0");
-//	test_get_float("-1.0     ");
-//	test_get_float(".0");
-//	test_get_float("		.0898");
-//	test_get_float("		.0898  lk lk");
-//	test_get_float("243");
+	test_get_double("-0.01234567890123456789 ");
+//	test_get_double("-0.09868798798798 ");
+//	test_get_double(" -0978  ");
+//	test_get_double("0.0");
+//	test_get_double("-1.0     ");
+//	test_get_double(".0");
+//	test_get_double("		.0898");
+//	test_get_double("		.0898  lk lk");
+//	test_get_double("243");
 
 }
 
@@ -201,15 +268,15 @@ t_vec3	str_get_vec3(const char *str, int *curs, int *err)
 	int		coma;
 	t_vec3	vec;
 
-	vec.x = str_get_float(str, &i);
+	vec.x = str_get_double(str, &i);
 	if ((coma = find_char(str + i, ',')) < 0)
 		return (vec3_null(*err = -1));
 	add = i + coma;
-	vec.y = str_get_float(str + add, &i);
+	vec.y = str_get_double(str + add, &i);
 	if ((coma = find_char(str + add + i, ',')) < 0)
 		return (vec3_null(*err = -1));
 	add += i + coma;
-	vec.z = str_get_float(str + add, &i);
+	vec.z = str_get_double(str + add, &i);
 	*curs = add + i;
 	return (vec);
 }
@@ -262,26 +329,119 @@ void	remove_coment(char *str)
 	}
 }
 
+/*
+**	Il sagit de prendre un mot et dire si quel valeur on incremente
+*/
+
+
+//	obj: 	plan, sphere, cylindre, cone
+//	light
+//	camera
+
+void	obj_add_value(t_obj *obj, char *str, int *curs)
+{
+
+}
+
+# define OBJ_TYPE 		0
+# define ASSETS_COMP	1
+# define CAM_COMP		2
+# define LIGHT_COMP		3
+
+
+char	*get_tab(int tab_nb, int *size)
+{
+	static	char	object_type[6][16] = {"light", "camera", "plan", "sphere", "cylinder", "cone"};
+	static	char	assets_comp[][16] = {"pos", "dir", "col", "ang"};
+	static	char	cam_comp[][16] = {"pos", "dir", "up" };
+	static	char	light_comp[][16] = {"pos", "col"};
+
+	if (!size)
+		return (NULL);
+	if (tab_nb == OBJ_TYPE && (*size = 6))
+		return ((char*) object_type);
+	if (tab_nb == ASSETS_COMP && (*size = 4))
+		return ((char*) assets_comp);
+	if (tab_nb == CAM_COMP && (*size = 3))
+		return ((char*) cam_comp);
+	if (tab_nb == LIGHT_COMP && (*size = 3))
+		return ((char*) light_comp);
+	return (NULL);
+}
+
+int		find_id_from_name(char *str, int tab_nb)
+{
+	int		i;
+	char	*toto;
+	int		size;
+
+	toto = get_tab(tab_nb, &size);
+	i = 0;
+	while (i < size)
+	{
+		if (!strcmp(toto + 16 * i, str))
+			return (i);
+		i++;
+	}
+	return (-1);
+}
+/*
+ 
+
+# define OBJ_TYPE 		0
+# define ASSETS_COMP	1
+# define CAM_COMP		2
+# define LIGHT_COMP		3
+
+ * */
+
 int	main()
 {
-//	test_float();
+//	test_double();
 //	test_vec3();
-	char *str = strdup("oui ----> #ceci est un comentaire\nEt la c'est normale ######\nYoupi\n#123456789\n");
 
-	str = strdup(
-"cone\n\
-{\n\
-	pos:	0, -3, 5\n\
-	col:	108, 139, 61\n\
-	dir:	0, 1, 0\n\
-	coef:	1\n\
-}"
-);
 
-	printf("%s\n", str);	
-//	printf("============ before =========:\n%s\n", str);
-//	remove_coment(str);
-//	printf("============ after  =========:\n%s\n", str);
-//	free (str);
+	char	*file_str = get_file_str();
+
+	remove_coment(file_str);
+//	printf("%s\n", file_str);
+
+	int	i;
+	int	j;
+	int	id_;
+
+	char truc[] = "plan";
+	printf("%s: %d\n", truc, find_id_from_name(truc, 0));
+	return (0);
+	i = 0;
+	while (file_str[i])
+	{
+		i += skip_withe_space(file_str + i);
+		j = skip_non_space(file_str + i);
+		i += j;
+	}
+	free (file_str);
 	return (0);
 }
+
+/*
+**	Tan qu'on est pas a la fin du fichier
+**		[ skip with space ]
+**		[ get obj_name ]
+**			[ skip with space ]
+**			[ get_param_name ]
+**				[get_param_value]
+**
+**				
+*/
+
+/*
+**
+**	On pourrait faire pour une structure un peu comme les base de donner:
+**		en gros des structure qui lie : mot_clef, fonction/indice de traitement, 
+*/
+
+
+// On pourais aussi faire pouvoir editer des parametre comme:
+// 		- taille de la fenetre
+// 		- vitesse: (translation, rotation)
