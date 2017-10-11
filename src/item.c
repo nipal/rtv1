f78daa6113a4a8b38131ade116a568295291756c
lapin <serprev.z@gmail.com>
2017-10-11 19:18:58 +0200

    little modification due to test the parsing part

--- src/item.c
+++ src/item.c
@@ -6,7 +6,7 @@
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/27 16:26:28 by fjanoty           #+#    #+#             */
-/*   Updated: 2017/10/07 12:15:19 by fjanoty          ###   ########.fr       */
+/*   Updated: 2017/10/11 19:18:28 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,16 +55,101 @@ void	old_item_init(t_item *item, t_mlx_win *w)
 
 }
 
+const	char	*obj_get_type_name(int id)
+{
+	switch (id)
+	{
+		case 0:
+			return ("plan");
+		case 1:
+			return ("sphere");
+		case 2:
+			return ("cylinder");
+		case 3:
+			return ("cone");
+	};
+	return ("--noting--");
+}
+
+void	obj_describe(t_obj *obj)
+{
+	printf("%s\n", obj_get_type_name(obj->type));
+	printf("{\n");
+	vec3_print_str(obj->pos, "	pos	");
+	vec3_print_str(obj->dir, "	dir	");
+	vec3_print_str(obj->col, "	col	");
+	printf("	value	%f\n", obj->value);
+	printf("}\n\n");
+}
+
+void	cam_describe2(t_cam *cam)
+{
+	printf("camera\n");
+	printf("{\n");
+	vec3_print_str(cam->pos, "	pos	");
+	vec3_print_str(cam->uz, "	uz	");
+	vec3_print_str(cam->uy, "	uy	");
+	vec3_print_str(cam->ux, "	ux	");
+	printf("}\n\n");
+}
+
+void	light_describe(t_light *light)
+{
+	printf("light\n");
+	printf("{\n");
+	vec3_print_str(light->pos, "	pos	");
+	vec3_print_str(light->col, "	col	");
+	printf("}\n\n");
+}
+
+void	item_describe(t_item *item)
+{
+	int	i;
+	printf("obj:%d	light:%d	cam:%d\n\n", item->nb_obj, item->nb_light, item->nb_cam);
+	i = 0;
+	while (i < item->nb_obj)
+		obj_describe(item->obj + i++);
+	i = 0;
+	while (i < item->nb_light)
+		light_describe(item->light + i++);
+	i = 0;
+	while (i < item->nb_cam)
+		cam_describe2(item->cam + i++);
+}
+
+void	init_obj_func(t_item *item)
+{
+	item->obj_dist[0] = get_dist_plan;
+	item->obj_dist[1] = get_dist_sphere;
+	item->obj_dist[2] = get_dist_cylinder;
+	item->obj_dist[3] = get_dist_cone;
+	item->obj_nrm[0] = get_normal_plan;
+	item->obj_nrm[1] = get_normal_sphere;
+	item->obj_nrm[2] = get_normal_cylinder;
+	item->obj_nrm[3] = get_normal_cone;
+}
+
 void	item_init(t_item *item, t_mlx_win *w, const char *file_path)
 {
-	(void)w;
-	int		file_size;
-	char	*str;
+	int			file_size;
+	char		*file_str;
+	t_entities	*lst;
 
-	if (!(str = file_str(file_path, &file_size)))
+	if (!(file_str = file_get_str(file_path, &file_size)))
 		rtv1_exit(get_env(NULL));
-//	rtv1_parse_file(str, file_size, item);
-	old_item_init(item, w);
+	if (!(lst = get_entities(file_str)))
+		rtv1_exit(get_env(NULL));
+	if (!(item_obj_alloc(lst, item)))
+		rtv1_exit(get_env(NULL));
+	item_fill(lst, item);
+	entities_destroy(lst);
+	item->size_x = w->size_x;
+	item->size_y = w->size_y;
+	free(file_str);
+	init_obj_func(item);
+//printf("\n===========================================\n");
+	item_describe(item);
+//	old_item_init(item, w);
 }
 
 void	item_destroy(t_item *it)
@@ -75,4 +160,6 @@ void	item_destroy(t_item *it)
 		free(it->light);
 	if (it->obj)
 		free(it->obj);
+	if (it->cam)
+		free(it->cam);
 }
