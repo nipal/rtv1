44966432a7292e99f83469b60cdaa49e0f02ce61
lapin <serprev.z@gmail.com>
2017-10-12 21:01:36 +0200

    fix the vec3_cross

--- quaternion/vec3.c
+++ quaternion/vec3.c
@@ -6,7 +6,7 @@
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/03 20:35:13 by fjanoty           #+#    #+#             */
-/*   Updated: 2017/10/10 22:11:40 by fjanoty          ###   ########.fr       */
+/*   Updated: 2017/10/12 17:42:36 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,9 +51,9 @@ t_vec3		vec3_cross(t_vec3 v1, t_vec3 v2)
 {
 	t_vec3	v3;
 
-	v3.x = v1.y * v2.z - v2.z * v2.y;
-	v3.y = v1.z * v2.x - v1.x * v2.z;
-	v3.z = v1.x * v2.y - v1.y * v2.x;
+	v3.x = (v1.y * v2.z) - (v1.z * v2.y);
+	v3.y = (v1.z * v2.x) - (v1.x * v2.z);
+	v3.z = (v1.x * v2.y) - (v1.y * v2.x);
 	return (v3);
 }
 
