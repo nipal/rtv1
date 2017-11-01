d437f219a439799b2851a50aa7251943d3ed4f1c
lapin <serprev.z@gmail.com>
2017-11-01 01:40:35 +0100

    fix the z_position of the line, then no more artefact with deps

--- src/post_processing.c
+++ src/post_processing.c
@@ -6,7 +6,7 @@
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/30 19:34:18 by fjanoty           #+#    #+#             */
-/*   Updated: 2017/10/31 18:06:23 by fjanoty          ###   ########.fr       */
+/*   Updated: 2017/11/01 01:38:59 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -59,36 +59,29 @@ void	pp_put_pix(t_mlx_win *w, t_vec3 pos, t_vec3 col)
 {
 	int	x;
 	int	y;
+	int	id;
 
 	x = (int)pos.x;
 	y = (int)pos.y;
-	if (x >= 0 && x < w->size_x && y >= 0 && y < w->size_y)
+	id = x + y * w->size_x;
+	if (mlx_win_is_inside(w, x, y) &&
+		(w->zbuff[id].dist < 0  || pos.z <= w->zbuff[id].dist))
 	{
-		w->data[x + y * w->size_x].nb = pp_vec_to_color(col);
-	} 
+		w->data[id].nb = pp_vec_to_color(col);
+		w->zbuff[id].dist = pos.z;
+		//	il faudrait aussi metre a jour d'autre valeur... ou pas
+	}
 }
 
 //	pos.x , pos.y => le centre le la sphere
 //	pos.z sont radius
 void	pp_draw_circle(t_mlx_win *w, t_vec3 pos, double radius, t_vec3 col)
 {
-	int	i;
-	int	j;
+	int		i;
+	int		j;
 	double	x;
 	double	y;
 
-//	t_obj	light;
-//	double	dist;
-//	double		(*obj_dist[4])(t_obj *obj, t_vec3 ray_pos, t_vec3 ray_dir);
-//	t_vec3	ray_pos;
-//	t_vec3	ray_dir;
-//	double	ratio;
-//	double	x1, y1;
-
-//	ratio = w->size_x / w->size_y;
-//	ray_pos = w->cam.pos; // en fait il faudrait la vrai position de la limire pour que ca fonctionne avec 
-//	light.pos = ;
-//	light.value = radius;
 	j = (int)(pos.y - (radius));
 	while (j < ((int)(pos.y + (radius))) && j >= 0 && j < w->size_y)
 	{
@@ -97,77 +90,99 @@ void	pp_draw_circle(t_mlx_win *w, t_vec3 pos, double radius, t_vec3 col)
 		{
 			x = i - pos.x;
 			y = j - pos.y;
-//			x1 = (double)i - (w->size_x / 2) * ratio;
-//			y1 = (double)j - (w->size_y / 2);
-//			ray_dir = vec3_normalise(vec3_add3(cam->uz, vec3_scalar(cam->ux, x1), vec3_scalar(cam->uy, y1)));
-//			printf("x:%f	y:%f: r:%f	==> pt:%f\n", x, y, radius, sum);
-			if ((w->z_buff[i + j * w->size_x].dist < 0  || pos.z <= w->z_buff[i + j * w->size_x].dist) && sqrt(x * x + y * y) <= radius)
-				pp_put_pix(w, vec3_set(i, j, 0), col);
+			if (sqrt(x * x + y * y) <= radius)
+				pp_put_pix(w, vec3_set(i, j, pos.z), col);
 			i++;
 		}
 		j++;
 	}
 }
 
-void	pp_draw_light(t_mlx_win *w, t_light *l, double radius, t_vec3 col)
+void	pp_draw_light_flat(t_mlx_win *w, t_light *l, double radius, t_vec3 col)
 {
-	//	On va representer la lumiere comme une sphere
-	//
-	//	dans le repaire de la camera: ou ce trouve la position?
-	//	----------------------------: quelle est la taille de la sphere?
-	//	parcourir le carre en question => en ne depassant pas du buffer_image
-	//	et avoir une fonction de coloration sympa
-	//	ET incruster la sphere selon le zbuffer
 	t_vec3	pos;
-//	int		i;
-//	int		j;
-//	int		id;
 
 	pos	= pp_screen_proj_coord(w, l->pos);
 	radius = (radius / pos.z) * w->size_y;
-//	pos.z = 5;
-
-//	vec3_print_str(pos, "pos_light_on_screen:");
-//	printf("radius_size:%.20f\n", radius);
-	// on pourrais aussi utiliser juste les fonction de pour tracer des sphere et comme ca on a tout
-	// genre position, profondeur, la vie
 	if (pos.z > 0)
 		pp_draw_circle(w, pos, radius, col);
-//	return ;
-//
-//	j = pos.y - (radius * 0.5);
-////	printf();
-//	while (j < (pos.y + (radius * 0.5)))
-//	{
-//		i = pos.x - radius * 0.5;
-//		while (i < (pos.x + radius * 0.5))
-//		{
-//			id = i + j * w->size_x;
-//			printf("i:%d	j:%d		id:%d\n", i, j, id);
-//			if (id >= 0 && id <= (w->size_x * w->size_y) && pp_is_circle(pos, radius, i, j))
-//			{
-//				w->data[id].nb = pp_get_light_color(radius, (double)i - pos.x, (double)j - pos.y, col);
-//			}
-//			i++;
-//		}
-//	}
-}
-//	on pourra comparer la taille avec une sphere que l'on raytrace
-
-void	pp_draw_segment(t_mlx_win *w, t_vec3 from, t_vec3 to, t_vec3 color)
-{
-	(void)w; (void)from;(void)to;(void)color;
-	//	dans le repaire de la camera: ou ce trouve les deux point?
-	//	Ou se trouve le debut et la fin... Est-ce qu'on dessine tout
-	//	parcourir le carre en question => en ne depassant pas du buffer_image
-	//	et avoir une fonction de coloration sympa
-	//	ET incruster la sphere selon le zbuffer
 }
 
 /*
-**	Ou est-ce qu'on stoque toute les ligne
-**	comment est-ce qu'on les cree
-**	comment on en ajoute
-**	comment en enleve? --> est-ce qu'on permet ca?
-**	c'est quoi l'instance qui les manage?
+**	En entrer on a deux point dans e repere du monde
+**	nous on va traduire ces coordoner dans le repere de la camera
+**	et on va tranquilement dessinner la ligne entre ces deux point
+**
+**	C'est dans une autre fonction que on va manager la creation, le stoquge
+**		et ordonner l'affichage, donc les appelle a cette fonction
+*/
+void	pp_draw_segment(t_mlx_win *w, t_vec3 from, t_vec3 to, t_vec3 color)
+{
+	int		i;
+	double	max;
+	t_vec3	dir_screen;
+	t_vec3	dir_world;
+	t_vec3	pt;
+
+	dir_world = vec3_sub(to, from);
+	pt = pp_screen_proj_coord(w, from);
+	to = pp_screen_proj_coord(w, to);
+	dir_screen = vec3_sub(to, pt);
+	max = 2 * (sqrt(dir_screen.x * dir_screen.x + dir_screen.y * dir_screen.y));
+	dir_screen = vec3_scalar(dir_screen, 1.0 / max);
+	dir_world = vec3_scalar(dir_world, 1.0 / max);
+	i = 0;
+	if (pt.z >= 0 && to.z >= 0
+			&& max < 10 * ((w->size_x > w->size_y) ? w->size_x : w->size_y))
+		while (i < max)
+		{
+			from = vec3_add(from, dir_world);
+			pt = pp_screen_proj_coord(w, from);
+			pt.z = vec3_norme(vec3_sub(from, w->cam->pos));
+			pp_put_pix(w, pt, color); 
+			i++;
+		}
+}
+
+void	pp_draw_segment2(t_mlx_win *w, t_vec3 from, t_vec3 to, t_vec3 color)
+{
+	int		i;
+	double	max;
+	t_vec3	dir;
+
+	to = pp_screen_proj_coord(w, to);
+	from = pp_screen_proj_coord(w, from);
+	dir = vec3_sub(to, from);
+	max = (sqrt(dir.x * dir.x + dir.y * dir.y));
+	dir = vec3_scalar(dir, 1.0 / max);
+	i = 0;
+	if (from.z >= 0 && to.z >= 0
+			&& max < 5 * ((w->size_x > w->size_y) ? w->size_x : w->size_y))
+		while (i < max)
+		{
+			from = vec3_add(from, dir);
+			pp_put_pix(w, from, color); 
+			i++;
+		}
+}
+
+
+	
+/*
+**	(1)	:Ou est-ce qu'on stoque toute les ligne
+**	(2)	:comment est-ce qu'on les cree
+**	(3)	:comment on en ajoute
+**	(4)	:comment en enleve? --> est-ce qu'on permet ca?
+**	(5)	:c'est quoi l'instance qui les manage?
+**	(6)	: A quelle moment, dans quel condition tout ca
+**			--> genre pour un normale a un obj...
+*/
+
+
+/*
+**	(1)	=>	on stoque tout dans des liste
+**	(2)	=>	avec une fonction 
+**	(3)	=>	foction de list: lst_add_front (pck + rapide)
+**	(4)	=>	on a des fonction generique de liste... 
+**	(5)	=>	un structur t_seg
 */
