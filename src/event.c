7e700d8d22e67205c01684277850272636e89791
lapin <serprev.z@gmail.com>
2017-11-04 09:01:38 +0100

    add key to switch between visual_debug (light and noral when click on onject) and normal view (wothout visual debug)

--- src/event.c
+++ src/event.c
@@ -6,7 +6,7 @@
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/18 15:22:06 by fjanoty           #+#    #+#             */
-/*   Updated: 2017/11/03 23:30:56 by fjanoty          ###   ########.fr       */
+/*   Updated: 2017/11/04 06:27:35 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,9 +40,12 @@ int		key_press(int key_code, t_mlx_win *w)
 	(key_code == KEY_J) ? cam_go_back(w->cam, dist) , w->refresh = 1: (void)w;
 	(key_code == KEY_K) ? cam_go_front(w->cam, dist) , w->refresh = 1: (void)w;
 	(key_code == KEY_P) ? cam_describe2(w->cam) : (void)w;
+	(key_code == KEY_TAB) ? cam_switch(w, &w->env->item)  , w->refresh = 1: (void)w;
+	(key_code == KEY_V) ? w->env->visual_debug++ : (void)w;
 //	(key_code == KEY_O) ? obj_describe(w->env->item.obj) : (void)w;
 //	(key_code == KEY_I) ? light_describe(w->env->item.light) : (void)w;
 //	mlx_put_image_to_window(w->env->mlx, w->env->scene.win, w->env->scene.img, 0, 0);
+	w->refresh = 1;
 	return (0);
 }
 
@@ -59,7 +62,8 @@ int		mousse_press(int button, int x, int y, t_mlx_win *w)
 	(void)w;
 	if (button == 1)
 	{
-		seg_add_obj_nrm(&w->env->item.all_segment, w, x, y);
+		if (w->env->visual_debug % 2)
+			seg_add_obj_nrm(&w->env->item.all_segment, w, x, y);
 		w->refresh = 1;
 	}
 //	printf("mousse_press:%d	{%d, %d}\n", button, x, y);
@@ -167,7 +171,8 @@ int		main_bcl(t_env *e)
 		e->scene.refresh = 0;
 		launch_ray(&e->scene, &e->item);
 		// on pourrai aussi faire un truc ou on ne dessinne les lumiere que quand on appui sur une touche
-		post_processing(e);
+		if (e->visual_debug % 2)
+			post_processing(e);
 		mlx_put_image_to_window(e->mlx, e->scene.win, e->scene.img, 0, 0);
 	}
 //	actual_time(e);
