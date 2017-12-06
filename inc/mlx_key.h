58d912a508e5b6838699b9709bd6360ad3465e2c
lapin <serprev.z@gmail.com>
2017-12-06 17:12:57 +0100

    finaly the cross compilation was wrong and simplyer to do =)

--- inc/mlx_key.h
+++ inc/mlx_key.h
@@ -6,21 +6,17 @@
 /*   By: fjanoty <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/20 21:20:43 by fjanoty           #+#    #+#             */
-/*   Updated: 2017/11/04 09:19:07 by fjanoty          ###   ########.fr       */
+/*   Updated: 2017/12/06 17:10:51 by fjanoty          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MLX_KEY_H
 # define MLX_KEY_H
 
-# ifdef OS_DARWIN_H
+# ifdef __APPLE__
 #  include "key_mac.h"
-# endif
-
-# ifdef OS_LINUX_H
+# elif __linux__
 #  include "key_linux.h"
 # endif
-
-#  include "key_linux.h"
 
 #endif
